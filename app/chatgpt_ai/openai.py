feddb15bf9eeb3b80d38784801d99d79b8cffa22
David Lohyňa <david.lohyna@gmail.com>
2023-03-10 23:43:01 +0100

    Initial Commit

---
+++ app/chatgpt_ai/openai.py
@@ -0,0 +1,22 @@
+import os
+
+import openai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+openai.api_key = os.getenv("CHATGPT_API_KEY")
+
+
+def chatgpt_response(prompt):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=1,
+        max_tokens=200
+
+    )
+    response_dict = response.get("choices")
+    if response_dict and len(response_dict) > 0:
+        prompt_response = response_dict[0]["text"]
+    return prompt_response
